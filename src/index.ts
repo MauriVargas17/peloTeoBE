2a53870933b567a486480402a357b32c78feafab
Sebastian Rojas <git-intern@econotec.com.bo>
2024-05-09 03:22:03 -0400

    test backend cicd

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ AppDataSource.initialize().then(async () => {
     app.use(express.json());
     app.use('/users', routes);
     app.use('/activity', actRoutes);
-    console.log('test')''
+    console.log('test');
 
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(error => {
